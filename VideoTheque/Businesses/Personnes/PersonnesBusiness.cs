90293cc56e3e9f2948a47933e6852b8480891384
Clément Cardot <clementcdt25@gmail.com>
2023-12-05 14:48:50 +0100

    implement Personne

---
+++ VideoTheque/Businesses/Personnes/PersonnesBusiness.cs
@@ -0,0 +1,57 @@
+﻿using VideoTheque.Core;
+using VideoTheque.DTOs;
+using VideoTheque.Repositories.Personnes;
+
+namespace VideoTheque.Businesses.Personnes
+{
+    public class PersonnesBusiness : IPersonnesBusiness
+    {
+        private readonly IPersonnesRepository _personneDao;
+
+        public PersonnesBusiness(IPersonnesRepository personneDao)
+        {
+            _personneDao = personneDao;
+        }
+
+        public Task<List<PersonneDto>> GetPersonnes() => _personneDao.GetPersonnes();
+
+        public PersonneDto GetPersonne(int id)
+        {
+            var personne = _personneDao.GetPersonne(id).Result;
+
+            if (personne == null)
+            {
+                throw new NotFoundException($"Personne '{id}' non trouvé");
+            }
+
+            return personne;
+        }
+
+        public PersonneDto InsertPersonne(PersonneDto personne)
+        {
+            if (_personneDao.InsertPersonne(personne).IsFaulted)
+            {
+                throw new InternalErrorException($"Erreur lors de l'insertion du personne {personne.Id}");
+            }
+
+            return personne;
+        }
+
+        public void UpdatePersonne(int id, PersonneDto personne)
+        {
+            if (_personneDao.UpdatePersonne(id, personne).IsFaulted)
+            {
+                throw new InternalErrorException($"Erreur lors de la modification du personne {personne.Id}");
+            }
+        }
+
+
+        public void DeletePersonne(int id)
+        {
+            if (_personneDao.DeletePersonne(id).IsFaulted)
+            {
+                throw new InternalErrorException($"Erreur lors de la suppression du personne d'identifiant {id}");
+            }
+        }
+    }
+}
---
+++ VideoTheque/Controllers/PersonnesController.cs
@@ -0,0 +1,52 @@
+﻿namespace VideoTheque.Controllers
+{
+    using Mapster;
+    using Microsoft.AspNetCore.Mvc;
+    using VideoTheque.Businesses.Personnes;
+    using VideoTheque.DTOs;
+    using VideoTheque.ViewModels;
+
+    [ApiController]
+    [Route("personnes")]
+    public class PersonnesController
+    {
+        private readonly IPersonnesBusiness _personnesBusiness;
+        protected readonly ILogger<PersonnesController> _logger;
+
+        public PersonnesController(ILogger<PersonnesController> logger, IPersonnesBusiness personnesBusiness)
+        {
+            _logger = logger;
+            _personnesBusiness = personnesBusiness;
+
+            TypeAdapterConfig<PersonneDto, PersonneViewModel>.NewConfig()
+                .Map(dest => dest.FullName, src => src.FirstName + " " + src.LastName);
+        }
+
+        [HttpGet]
+        public async Task<List<PersonneViewModel>> GetPersonnes() => (await _personnesBusiness.GetPersonnes()).Adapt<List<PersonneViewModel>>();
+
+        [HttpGet("{id}")]
+        public async Task<PersonneViewModel> GetPersonne([FromRoute] int id) => _personnesBusiness.GetPersonne(id).Adapt<PersonneViewModel>();
+
+        [HttpPost]
+        public async Task<IResult> InsertPersonne([FromBody] PersonneViewModel personneVM)
+        {
+            var created = _personnesBusiness.InsertPersonne(personneVM.Adapt<PersonneDto>());
+            return Results.Created($"/personnes/{created.Id}", created);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IResult> UpdatePersonne([FromRoute] int id, [FromBody] PersonneViewModel personneVM)
+        {
+            _personnesBusiness.UpdatePersonne(id, personneVM.Adapt<PersonneDto>());
+            return Results.NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IResult> DeletePersonne([FromRoute] int id)
+        {
+            _personnesBusiness.DeletePersonne(id);
+            return Results.Ok();
+        }
+    }
+}
